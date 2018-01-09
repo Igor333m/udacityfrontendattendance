57b6821febe1c801d5b6fa0e9ab353fe93328101
DESKTOP-TUVHL3G\Lokiji <igor333m@yahoo.com>
2018-01-09 09:25:21 -0800

    refactor: Add randomAttendance and countMissing

--- js/apprefactor.js
+++ js/apprefactor.js
@@ -3,23 +3,23 @@ let model = {
 	students: [
 		{
 			name: "Slappy the Frog",
-			attendance: null
+			attendance: []
 		},
 		{
 			name: "Lilly the Lizard",
-			attendance: null
+			attendance: []
 		},
 		{
 			name: "Paulrus the Walrus",
-			attendance: null
+			attendance: []
 		},
 		{
 			name: "Gregory the Goat",
-			attendance: null
+			attendance: []
 		},
 		{
 			name: "Adam the Anaconda",
-			attendance: null
+			attendance: []
 		}
 	]
 }
@@ -28,7 +28,9 @@ let model = {
 let controller = {
 
 	init: () => {
+		view.randomAttendance();
 		view.init();
+		view.countMissing();
 	},
 
     getRandom: () => {
@@ -43,32 +45,57 @@ let controller = {
 
 /********************************************************************/
 let view = {
+	// Populate table, check boxes, based on attendace records
 	init: () => {
 		this.nameColumns = document.querySelector("tbody");
 		this.containerTbody = "";
 		controller.getAllStudents().forEach( student => {
 			containerTbody += `
 				<tr class="student">
-					<td class="name-col">${student.name}</td>
-					<td class="attend-col"><input type="checkbox"></td>
-					<td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
-                    <td class="attend-col"><input type="checkbox"></td>
+					<td class="name-col">${student.name}</td>`
+			student.attendance.forEach( att => {
+				if (att) {
+					containerTbody += `<td class="attend-col"><input type="checkbox" checked></td>`
+				}else {
+					containerTbody += `<td class="attend-col"><input type="checkbox"></td>`
+				}
+			});
+				containerTbody += `
                     <td class="missed-col">0</td>
-				</tr>
-			`;
+				</tr>`
+			;
 		});
 		
 		this.nameColumns.innerHTML = containerTbody;
-	}
+	},
+
+	randomAttendance: () => {
+        for(let i = 0; i < controller.getAllStudents().length; i++) {
+            for (var k = 0; k <= 11; k++) {
+                model.students[i].attendance.push(controller.getRandom());
+            }
+        };
+	},
+
+	// Count a student's missed days
+    countMissing: () => {
+    	$allMissed = $('tbody .missed-col');
+        $allMissed.each(function() {
+            var studentRow = $(this).parent('tr'),
+                dayChecks = $(studentRow).children('td').children('input'),
+                numMissed = 0;
+
+            dayChecks.each(function() {
+                if (!$(this).prop('checked')) {
+                    numMissed++;
+                }
+            });
+
+            $(this).text(numMissed);
+        });
+    }
+
+	
 }
 
 controller.init();
