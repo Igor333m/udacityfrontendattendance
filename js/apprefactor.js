54f28099b6233382bfd94513b880ad9b81b0a9cb
DESKTOP-TUVHL3G\Lokiji <igor333m@yahoo.com>
2018-01-09 08:32:36 -0800

    refactor: MCV add

--- js/apprefactor.js
+++ js/apprefactor.js
@@ -1,4 +1,61 @@
 /* Model with all data */
 let model = {
-	studentNames: null
+	students: [
+		{
+			name: "Slappy the Frog",
+			attendance: null
+		},
+		{
+			name: "Lilly the Lizard",
+			attendance: null
+		},
+		{
+			name: "Paulrus the Walrus",
+			attendance: null
+		},
+		{
+			name: "Gregory the Goat",
+			attendance: null
+		},
+		{
+			name: "Adam the Anaconda",
+			attendance: null
+		}
+	]
 }
+
+/********************************************************************/
+let controller = {
+
+	init: () => {
+		view.init();
+	},
+
+    getRandom: () => {
+    	console.log('Creating attendance records...');
+        return (Math.random() >= 0.5);
+    },
+
+    getAllStudents: () => {
+    	return model.students;
+    }
+}
+
+/********************************************************************/
+let view = {
+	init: () => {
+		this.nameColumns = $("tbody");
+		controller.getAllStudents().forEach( student => {
+			containerTbody = `
+				<tr class="student">
+					<td class="name-col">${student.name}</td>
+					<td class="attend-col"><input type="checkbox"></td>
+				</tr>
+			`;
+		});
+		
+		document.querySelector(".student").insertAdjacentHTML("afterbegin", containerTbody);
+	}
+}
+
+controller.init();
